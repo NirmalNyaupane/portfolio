35f5e49488ae798117ae51e0537b6c0bb9a11a38
Nirmal Neupane <neupanenirmal417@gmail.com>
2025-02-22 12:24:44 +0545

    feat: contact us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Experience from "../components/Experience"
 import Skills from "../components/Skills"
 import Projects from "../components/Projects"
 import Footer from "../components/Footer"
+import Contact from "../components/contact"
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Skills />
       <Experience />
       <Projects />
+      <Contact />
       <Footer />
     </main>
   )
